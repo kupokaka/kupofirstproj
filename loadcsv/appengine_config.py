39b4d084100a72f7d10be618e65a3808b29b8203
kupokaka <45750170+kupokaka@users.noreply.github.com>
2018-12-10 11:57:01 +0530

    Update appengine_config.py

--- loadcsv/appengine_config.py
+++ loadcsv/appengine_config.py
@@ -1,15 +1,6 @@
 import os
 from google.appengine.ext import vendor
 
-# Adding any third party libraries that are not built into the
-# bundled GAE runtime.
-#
-# install libs with: pip install -t lib <library_name>
-#
-# Also can use requirements.txt to list libs
-#    and then run: pip install -t lib -r requirements.txt
-#
-# Add any libraries installed in the "lib" folder.
-#
+
 vendor.add(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib'))
 
